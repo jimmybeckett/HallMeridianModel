115133682903a4b20884cd9b0121ee862d7f9416
Jimmy Beckett <jimmybeckett1@gmail.com>
2017-09-28 13:18:26 -0500

    Add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,7 @@ public class Main {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(Main.class);
         Spark.staticFileLocation("/webapp");
+        port(getHerokuAssignedPort());
 
         get("/getSSTimes", (req, res) -> {
             logger.info("get request to /getSSTimes");
@@ -31,5 +32,13 @@ public class Main {
                 return 500;
             }
         }, new JsonUtil());
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
