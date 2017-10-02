ea2afd6eaa9345c7e9bc7252cfdb3167311fc33c
Jimmy Beckett <jimmybeckett1@gmail.com>
2017-10-01 23:08:31 -0500

    Add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,7 +16,7 @@ public class Main {
 
         get("/getSSTimes", (req, res) -> {
             logger.info("get request to /getSSTimes");
-            res.header("Access-Control-Allow-Origin", "https://hallmeridianmodel.herokuapp.com/");
+            res.header("Access-Control-Allow-Origin", "*");
             try {
                 Calendar calendar = Calendar.getInstance();
                 calendar.setTimeInMillis(Long.parseLong(req.headers("millis")));
